0d93d4166481d265196048c8bdaf797c58503fd5
deanmajaya <deanmajaya@gmail.com>
2021-12-10 13:12:27 +0200

    comparing time to run sum of list object to sum of generator object

---
+++ compare.py
@@ -0,0 +1,33 @@
+# compares the running time of a list to a generator
+import time
+
+def oddGen(n, m) -> None:
+    '''
+    generator function creates an iterator of odd numbers
+    between n and m
+    '''
+
+    while n < m:
+        yield n
+        n += 2
+
+def oddLst(n, m) -> None:
+    '''
+    builds a list of odd numbers between n and m
+    '''
+
+    lst = []
+    while n < m:
+        lst.append(n)
+        n += 2
+    return lst
+
+# time it takes to perform sum on an generator
+t1 = time.time()
+sum(oddGen(1, 1000000))
+print('time to sum a generator: %f' % (time.time() - t1))
+
+# time it takes to build and sum a list
+t1=time.time()
+sum(oddLst(1, 1000000))
+print('time to build and sum a list: %f' % (time.time() - t1))
